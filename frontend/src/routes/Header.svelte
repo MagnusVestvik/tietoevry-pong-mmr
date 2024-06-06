626b6ff8a9ddd9c126f9bccf6051d41bfaf3f9f6
magnus <magnus.vestvik@tietoevry.com>
2024-06-06 15:33:15 +0200

    feat add frontend template

---
+++ frontend/src/routes/Header.svelte
@@ -0,0 +1,33 @@
+<script>
+</script>
+
+<header>
+	<div class="flex flex-row">
+		<iframe
+			src="https://giphy.com/embed/qWY66aDGRM8quftLGY"
+			width="30%"
+			height="30%"
+			style=""
+			frameBorder="0"
+			class="giphy-embed"
+			allowFullScreen
+		></iframe>
+		<h1 class="seymour-one-regular">TietoEvry Pong MMR</h1>
+	</div>
+</header>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Seymour+One&display=swap');
+
+	.seymour-one-regular {
+		font-family: 'Seymour One', sans-serif;
+		font-weight: 400;
+		font-style: normal;
+	}
+
+	h1 {
+		font-family: 'Seymour One', sans-serif;
+		font-size: 2rem;
+		text-align: center;
+	}
+</style>
