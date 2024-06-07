8d64aea1f299e34ac13cdc846b650871742ba250
magnus <magnus.vestvik@tietoevry.com>
2024-06-07 14:36:28 +0200

    feat reinit frontend
    changed frontend to use the default cli setup with skeleton ui

---
+++ frontend/pong-mmr/tailwind.config.js
@@ -0,0 +1,27 @@
+import { join } from 'path'
+
+import { skeleton } from '@skeletonlabs/tw-plugin'
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'rocket',
+						enhancements: true,
+					},
+					{
+						name: 'hamlindigo',
+						enhancements: true,
+					},
+				],
+			},
+		}),
+	],
+};
