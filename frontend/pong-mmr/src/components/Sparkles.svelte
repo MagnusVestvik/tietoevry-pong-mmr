2281d48a66628973d99151e3d370df023f517064
Magnus <magnus_95_vestvik@hotmail.com>
2024-06-08 13:15:58 +0200

    feat add Sparkles component
    Sparkle component inspired by aceternity ui, IT IS NOT DONE YET.

---
+++ frontend/pong-mmr/src/components/Sparkles.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { onMount } from 'svelte';
+
+	/**
+	 * @type {string}
+	 */
+	export let height;
+
+	/**
+	 * @type {string}
+	 */
+	export let width;
+
+	/**
+	 * @type {string}
+	 */
+	export let backgroundColor;
+
+	/**
+	 * @type {string[]}
+	 */
+	let sparklesArr = [];
+
+	onMount(() => {
+		let div = document.createElement('div');
+		div.className = 'w-1 h-1 bg-white rounded-full';
+		sparklesArr = Array(100)
+			.fill('')
+			.map(() => div.outerHTML);
+	});
+</script>
+
+<div style="width: {width}; height: {height}; background-color: {backgroundColor};">
+	{#each sparklesArr as sparkle}
+		{@html sparkle}
+	{/each}
+</div>
