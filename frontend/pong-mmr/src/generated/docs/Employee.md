0686af4203c640f9761d1ed1ec135cd86cadb3b5
magnus <magnus.vestvik@tietoevry.com>
2024-06-24 16:51:00 +0200

    feat add match route
    match route should be used when wanting to register the result of a new match or when a match is in progress.

--- frontend/pong-mmr/src/generated/docs/Employee.md
+++ frontend/pong-mmr/src/generated/docs/Employee.md
@@ -1,4 +1,5 @@
 # TietoevryPongMmr.Employee
+  
 
 ## Properties
 Name | Type | Description | Notes
---
+++ frontend/pong-mmr/src/routes/match/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="flex flex-col w-screen h-screen">
+	<div class="flex flex-row items-center justify-center w-[100%] h-[100%]">
+		<div class="card p-4 m-4">
+			<header class="card-header">
+				<Avatar
+					src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop"
+					width="w-32"
+					rounded="rounded-full"
+				/>
+			</header>
+			<section class="p-4">Sothing somthing</section>
+			<footer>Score</footer>
+		</div>
+		<div class="card p-4 m-10">
+			<header class="card-header">
+				<Avatar
+					src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop"
+					width="w-32"
+					rounded="rounded-full"
+				/>
+			</header>
+			<section class="p-4">Sothing somthing</section>
+			<footer>Score</footer>
+		</div>
+	</div>
+</div>
